a371fdb777aa0c5895bacd29a514e61781615f79
ruivergani <ruiverganineto@gmail.com>
2024-05-03 17:37:09 +0100

    component card dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,16 @@ import ContainerGrid from "@/components/container";
 import TitleSection from "@/components/title-section";
 import CardNFT from "@/components/card-nft";
 
+import ImageNft01 from '@/assets/img-neft/01.jpg'
+
 export default function Home() {
   return (
     <ContainerGrid className="flex">
-      <CardNFT>
-        
+      <CardNFT
+        thumbnail={ImageNft01}
+        value={0.0721}
+        value_BRL={602.02}
+      >
       </CardNFT>
     </ContainerGrid>
   );
--- src/components/card-nft.js
+++ src/components/card-nft.js
@@ -1,17 +1,16 @@
 import Image from 'next/image';
 
-import ImageNft01 from '@/assets/img-neft/01.jpg'
 import IconHeart from '@/assets/icon-heart.svg'
 import IconVerify from '@/assets/verify.svg'
 import IconEtherum from '@/assets/icon-eth.svg'
 import IconClock from '@/assets/icon-clock.svg'
 
-export default function CardNFT({imagePath, title}) {
+export default function CardNFT({thumbnail,name, value, value_BRL}) {
   return (
     <div className='w-full max-w-card-ntf overflow-hidden rounded-2xl border border-green-border-card bg-background-card'>
       <div className='w-full h-image-area-nft overflow-hidden relative'>
         <Image
-          src={ImageNft01}
+          src={thumbnail}
           alt='Nft 01'
           className='w-full h-full object-cover'
         >
@@ -26,17 +25,17 @@ export default function CardNFT({imagePath, title}) {
           Cartoon Collection
           <Image src={IconVerify} alt='icon verify'></Image>
         </h4>
-        <h3 className='text-lg font-semibold mb-4'>Cat #221</h3>
+        <h3 className='text-lg/none font-semibold mb-4'>{name}</h3>
         <div className='flex justify-between items-end'>
           <div>
             <span className='text-xs text-white opacity-70'>Actual Price</span>
-            <h3 className='flex items-center justify-center gap-1 font-semibold mt-1'><Image src={IconEtherum} alt='icon verify'></Image>0.0721 BTC</h3>
+            <h3 className='flex items-center justify-center gap-1 font-semibold mt-1'><Image src={IconEtherum} alt='icon verify'></Image>{value} BTC</h3>
           </div>
-          <h5 className='text-xs relative bottom-1'>$602,00</h5>
+          <h5 className='text-xs relative bottom-1'>R$ {value_BRL}</h5>
         </div>
       </div>
-      <button className='w-full max-w-[248px] leading-[48px] mx-auto flex items-center justify-center px-20 bg-background-button-nft rounded-[122px] mb-4 hover:brightness-75 transition'>Buy Now</button>
-      <div className='mt-4 space-x-2 flex flex-row ml-4'>
+      <button className='w-full max-w-[248px] leading-[48px] mx-auto flex items-center justify-center px-20 bg-background-button-nft rounded-[122px] mb-4 hover:brightness-75 transition ease-linear'>Buy Now</button>
+      <div className='mt-4 space-x-2 flex flex-row ml-4 items-center justify-start mb-4'>
         <Image src={IconClock} alt='icon clock'></Image>
         <p className='text-xs text-white opacity-80'>End in 4h 8m 22s</p>
       </div>
